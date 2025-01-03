6e17a701ea378cb1f6f416b0066f9265e2a82dc5
Gbesh <gbesh.12@gmail.com>
2025-01-03 18:37:21 +0000

    static files

--- credit_union/credit_union/settings.py
+++ credit_union/credit_union/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-#&"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["credit-union.onrender.com"]
 
 
 # Application definition
